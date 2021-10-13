3539dd2e20a9b8f7c066ea67dd6be3c7c71851cf
Damian Demasi <damian@amplify-now.com>
2021-10-14 09:28:51 +1030

    DB seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_10_13_225349) do
+
+  create_table "beers", force: :cascade do |t|
+    t.string "brand"
+    t.string "style"
+    t.string "country"
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Beer.create(brand: 'Double Stout', style: 'Stout', country: 'England', quantity: 54)
+Beer.create(brand: 'Spaten', style: 'Helles', country: 'Germany', quantity: 3)
+Beer.create(brand: 'Newcastle', style: 'Brown ale', country: 'UK', quantity: 12)
