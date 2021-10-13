04d7bb6711b6b01acc0c696a14e8e4c1daeceecd
Damian Demasi <damian@amplify-now.com>
2021-10-14 10:14:45 +1030

    Routes pointing to the API Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'beers/index'
+      post 'beers/create'
+      delete 'beers/:id', to: 'beers#destroy'
+    end
+  end
+  
   root 'beers#index'
   
   # resources :beers
