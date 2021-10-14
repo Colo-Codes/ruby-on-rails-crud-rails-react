acbd36e3e398963f76073b7a1034b2d84f6f4f51
Damian Demasi <damian@amplify-now.com>
2021-10-14 12:19:52 +1030

    Possible update route

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'beers/index'
       post 'beers/create'
+      # put 'beers/:id', to: 'beers#update'
       delete 'beers/:id', to: 'beers#destroy'
     end
   end
-  
+
   root 'beers#index'
   
   # resources :beers
